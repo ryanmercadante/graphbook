570d67f6b823c1362c2b34f889f78b66e7f3b497
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-03-25 16:18:09 -0400

    fixing the id's of the data already in mysql

--- src/server/seeders/20190323135817-fake-posts.js
+++ src/server/seeders/20190323135817-fake-posts.js
@@ -6,19 +6,23 @@
 // The QueryInterface of Sequelize is the general interface that Sequelize uses to talk to all databaes.
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Posts', [{
-      text: 'Lorem Ipsum 1',
-      userId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      text: 'Lorem Ipsum 2',
-      userId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }],
-  {});
+    // Get all existing users
+    return queryInterface.sequelize.query('SELECT id from Users;').then(users => {
+      const usersRows = users[0]
+      return queryInterface.bulkInsert('Posts', [{
+        text: 'Lorem Ipsum 1',
+        userId: usersRows[0].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        text: 'Lorem Ipsum 2',
+        userId: usersRows[1].id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }],
+      {});
+    });
   },
 
   // The down migration bulk deletes all rows in the table
